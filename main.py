04c28e822b28720470b498bcca04612d094d6745
Al_Pl <asuspitsyn@mail.ru>
2024-05-18 01:01:16 +0300

    added poetry.lock

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-from src.addit_for_10.1 import list_sort_prod_price, quant_avg_pr_month
-from src.addit_for_9.2 import count_words, list_of_words_in_file
+from src.addit_for_10_first import list_sort_prod_price, quant_avg_pr_month
+from src.addit_for_9 import count_words, list_of_words_in_file
