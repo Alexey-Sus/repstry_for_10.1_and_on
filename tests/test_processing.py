10816daeb6d9aabe2849dcfdbdabe8df4bcab4df
Al_Pl <asuspitsyn@mail.ru>
2024-05-23 00:31:14 +0300

    added a fixture for testing the two list funcitons (from lesson 10.1); added a parametrization function for a function from lesson 9.2; ran the code coverage feature with the 99% overall result

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -9,26 +9,48 @@ list_orig: list = [
     {"id": 615064591, "state": "CANCELLED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
+
 def test_list_sort_date():
     # переменная, которая должна быть возвращена:
     var_for_output: list = [
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELLED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELLED', 'date': '2018-10-14T08:21:33.419441'},
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELLED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELLED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
     ]
     assert list_sort_date(list_orig, False) == var_for_output
     assert list_sort_date([]) == []
     assert list_sort_date('343434') == []
     assert list_sort_date({}) == []
 
+
 def test_list_for_state():
     # переменная, которая должна быть возвращена:
     var_for_output: list = [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-        ]
+    ]
     assert list_for_state(list_orig) == var_for_output
     assert list_for_state([]) == []
     assert list_for_state('') == []
     assert list_for_state({}) == []
+
+
+# тестируем обе функции с помощью их фикстуры из файла conftest.py
+
+def test_list_sort_date_1(fixt_for_lists):
+    var_for_output: list = [
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELLED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELLED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}
+    ]
+    assert list_sort_date(fixt_for_lists, False) == var_for_output
+
+
+def test_list_for_state_1(fixt_for_lists):
+    var_for_output: list = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
+    assert list_for_state(fixt_for_lists) == var_for_output
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,15 +1,35 @@
 from src.widget import get_date, mask_for_acc_card
+import pytest
 
 
 def test_mask_for_acc_card():
     str_new: str = "Visa Gold 8976700079228958"
-    assert mask_for_acc_card(str_new) == 'Карта Visa Gold 8976 70** **** 8958'
-    assert mask_for_acc_card('') == ''
+    assert mask_for_acc_card(str_new) == "Карта Visa Gold 8976 70** **** 8958"
+    assert mask_for_acc_card("") == ""
+
+
+# определяем фикстуру для функции test_get_date:
 
 
 def test_get_date():
-    date_for_ex: str = '2018-12-11T02:26:18.671407'
-    assert get_date(date_for_ex) == '11.12.18'
-    assert get_date('') == ''
-    assert get_date([]) == ''
-    assert get_date({}) == ''
+    date_for_ex: str = "2018-12-11T02:26:18.671407"
+    assert get_date(date_for_ex) == "11.12.18"
+    assert get_date("") == ""
+    assert get_date([]) == ""
+    assert get_date({}) == ""
+
+
+# тестируем функцию get_date с помощью параметризации:
+@pytest.mark.parametrize(
+    "input_date, expected_date",
+    [
+        ("2034-07-15T02:26:18.671407", "15.07.34"),
+        ("1977-01-02T02:26:18.671407", "02.01.77"),
+        (3434343, ""),
+        ("", ""),
+        ([], ""),
+        ({}, "")
+    ],
+)
+def test_get_date_1(input_date, expected_date):
+    assert get_date(input_date) == expected_date
